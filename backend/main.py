7e42f3a4f6a4d9270bd2a5dc924a5febc760e73d
Joel Perez <joel@joelperez.dev>
2020-11-03 16:28:14 -0700

    finished alerts db model

--- backend/main.py
+++ backend/main.py
@@ -5,30 +5,42 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-# SQL Table Migration Classes
+# # SQL Table Migration Classes
 class ContactsModel(db.Model):
     _id = db.Column(db.String, primary_key=True)
-    contactId = db.Column(db.String(40))
-    contactStatus = db.Column(db.String(20))
-    contactName = db.Column(db.Integer(12))
+    contactId = db.Column(db.String(48))
+    contactStatus = db.Column(db.String(48))
+    contactName = db.Column(db.Integer())
     contactGround = db.Column(db.String(36))
     contactSatellite = db.Column(db.String(24))
     contactEquipment = db.Column(db.String(128))
     contactState = db.Column(db.String(24))
     contactStep = db.Column(db.String(48))
     contactDetail = db.Column(db.String(512))
-    contactBeginTimestamp = db.Column(db.Integer(24))
-    contactEndTimestamp = db.Column(db.Integer(24))
+    contactBeginTimestamp = db.Column(db.DateTime())
+    contactEndTimestamp = db.Column(db.DateTime())
     contactLatitude = db.Column(db.Float(12))
     contactLongitute = db.Column(db.Float(12))
     contactAzimuth = db.Column(db.Float(12))
     contactElevation = db.Column(db.Float(12))
     contactResolution = db.Column(db.String(48))
     contactResolutionStatus = db.Column(db.String(48))
-    
+
+class Alerts(db.Model):
+    errorId = db.Column(db.String(48), primary_key=True)
+    errorSeverity = db.Column(db.String(48))
+    errorCategory = db.Column(db.String(48))
+    errorMessage = db.Column(db.String(128))
+    longMessage = db.Column(db.String(256))
+    errorTime = db.Column(db.DateTime())
+    selected = db.Column(db.Boolean())
+    new = db.Column(db.Boolean())
+    expanded = db.Column(db.Boolean())
+
 
 
 # Contacts endpoint
