9b13efd2c49fae50734639acc6ebed7d64df22af
Joel Perez <joel@joelperez.dev>
2020-11-09 12:55:15 -0700

    WIP: Adding Contacts Table

---
+++ frontend/src/Components/ContactTable.js
@@ -0,0 +1,395 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import moment from "moment";
+import { lighten, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+// import IconButton from "@material-ui/core/IconButton";
+// import Tooltip from "@material-ui/core/Tooltip";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Switch from "@material-ui/core/Switch";
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import FilterListIcon from '@material-ui/icons/FilterList';
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData("Cupcake", 305, 3.7, 67, 4.3),
+//   createData("Donut", 452, 25.0, 51, 4.9),
+//   createData("Eclair", 262, 16.0, 24, 6.0),
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9),
+//   createData("Honeycomb", 408, 3.2, 87, 6.5),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+//   createData("Jelly Bean", 375, 0.0, 94, 0.0),
+//   createData("KitKat", 518, 26.0, 65, 7.0),
+//   createData("Lollipop", 392, 0.2, 98, 0.0),
+//   createData("Marshmallow", 318, 0, 81, 2.0),
+//   createData("Nougat", 360, 19.0, 9, 37.0),
+//   createData("Oreo", 437, 18.0, 63, 4.0),
+// ];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+// const headCells = [
+//   {
+//     id: "name",
+//     numeric: false,
+//     disablePadding: true,
+//     label: "Dessert (100g serving)",
+//   },
+//   { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
+//   { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
+//   { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
+//   { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" },
+// ];
+
+function EnhancedTableHead(props) {
+  const {
+    classes,
+    // onSelectAllClick,
+    order,
+    orderBy,
+    // numSelected,
+    // rowCount,
+    onRequestSort,
+    columns,
+  } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {/* <TableCell padding="checkbox">
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{ "aria-label": "select all desserts" }}
+          />
+        </TableCell> */}
+        {columns.map((column) => (
+          <TableCell
+            key={column.id}
+            align={column.numeric ? "right" : "left"}
+            padding={column.disablePadding ? "none" : "default"}
+            sortDirection={orderBy === column.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === column.id}
+              direction={orderBy === column.id ? order : "asc"}
+              onClick={createSortHandler(column.id)}
+            >
+              {column.label}
+              {orderBy === column.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === "light"
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: "1 1 100%",
+  },
+}));
+
+const EnhancedTableToolbar = (props) => {
+  const classes = useToolbarStyles();
+  const { numSelected, totals } = props;
+
+  return (
+    <Toolbar className="rux-table th">
+      {/* <Typography
+            className={classes.title}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+        >
+            Contacts
+        </Typography> */}
+      <Grid container spacing={1} direction="row" wrap="nowrap">
+        <Grid container item spacing={0} direction="column">
+          <h2>{totals.contacts}</h2>
+          <p>Contacts</p>
+        </Grid>
+        <Grid container item spacing={0} direction="column">
+          <h2>{totals.failed}</h2>
+          <p>Failed</p>
+        </Grid>
+        <Grid container item spacing={0} direction="column">
+          <h2>{totals.executing}</h2>
+          <p>Executing</p>
+        </Grid>
+      </Grid>
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  paper: {
+    width: "100%",
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function ContactTable(props) {
+  const { rows, columns } = props;
+
+  const classes = useStyles();
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("calories");
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  const findTotals = (data) => {
+    const totals = {
+      executing: 0,
+      failed: 0,
+      contacts: data.length,
+    };
+    data.forEach((contact) => {
+      totals[contact.contactState] += 1;
+    });
+    return totals;
+  };
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar
+          numSelected={selected.length}
+          totals={findTotals(rows)}
+        />
+        <TableContainer>
+          <Table
+            className="rux-table"
+            aria-labelledby="tableTitle"
+            size={dense ? "small" : "medium"}
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+              columns={columns}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                        className="rux-table tr"
+                      hover
+                      onClick={(event) => handleClick(event, row.name)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      {/* <TableCell padding="checkbox">
+                        <Checkbox
+                          checked={isItemSelected}
+                          inputProps={{ "aria-labelledby": labelId }}
+                        />
+                      </TableCell> */}
+                      <TableCell
+                        component="th"
+                        className="rux-table tr"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.contactName}
+                      </TableCell>
+                      <TableCell align="right" className="rux-table tr">{row.contactStatus}</TableCell>
+                      <TableCell align="right" className="rux-table tr">
+                        {moment(row.contactBeginTimestamp).format("hh:mm:ss")}
+                      </TableCell>
+                      <TableCell align="right" className="rux-table tr">
+                        {moment(row.contactEndTimestamp).format("hh:mm:ss")}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          className="rux-table th"
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+      {/* <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      /> */}
+    </div>
+  );
+}
---
+++ frontend/src/Components/Dashboard.js
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from "react"
+import Grid from "@material-ui/core/Grid"
+
+import axiosWithAuth from "../utils/axiosWithAuth"
+import ContactTable from "./ContactTable"
+
+const contactColumns = [
+    { id: 'contactName', numeric: false, disablePadding: true, label: 'Name' },
+    { id: 'contactStatus', numeric: false, disablePadding: false, label: 'Status' },
+    { id: 'contactBeginTimestamp', numeric: false, disablePadding: false, label: 'Begin Timestamp' },
+    { id: 'contactEndTimestamp', numeric: false, disablePadding: false, label: 'End Timestamp' },
+  ];
+  
+
+const Dashboard = props => {
+    const [alerts, setAlerts] = useState([])
+    const [contacts, setContacts] = useState([])
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get("alerts")
+            .then(res => {
+
+                setAlerts(res.data)
+            })
+            .catch(error => {
+                console.error(error.response)
+            })
+        axiosWithAuth()
+            .get("contacts")
+            .then(res => {
+                console.log(res.data[0])
+                setContacts(res.data)
+            })
+            .catch(error => {
+                console.error(error.response)
+            })
+    }, [])
+
+    return (
+        <div className="dashboard-container">
+            <h1>Welcome to the Dashboard!</h1>
+            <Grid container spacing={1}>
+                <Grid item>
+                    <ContactTable
+                        columns={contactColumns}
+                        rows={contacts}
+                    />
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Dashboard
