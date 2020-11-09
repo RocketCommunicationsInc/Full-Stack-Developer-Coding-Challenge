2f32f8d7ca82080469d7b1b33777a1f6eeb42ac8
Joel Perez <joel@joelperez.dev>
2020-11-09 15:56:56 -0700

    bug: If API reboots db will seed again. commented out seed code

--- backend/main.py
+++ backend/main.py
@@ -26,47 +26,47 @@ db.init_app(app)
 
 # ---!!! Create Database !!!---
 # NOTE: Will create and seed database if not found on first API request
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    contacts = json.load(open("../contacts.json"))
-    alerts = json.load(open("../alerts.json"))
-    for i in contacts:
-        contact = ContactsModel(
-        _id=i["_id"],
-        contactId=i["contactId"],
-        contactStatus=i["contactStatus"],
-        contactName=i["contactName"],
-        contactGround=i["contactGround"],
-        contactSatellite=i["contactSatellite"],
-        contactEquipment=i["contactEquipment"],
-        contactState=i["contactState"],
-        contactStep=i["contactStep"],
-        contactDetail=i["contactDetail"],
-        contactBeginTimestamp=i["contactBeginTimestamp"],
-        contactEndTimestamp=i["contactEndTimestamp"],
-        contactLatitude=i["contactLatitude"],
-        contactLongitude=i["contactLongitude"],
-        contactAzimuth=i["contactAzimuth"],
-        contactElevation=i["contactElevation"],
-        contactResolution=i["contactResolution"],
-        contactResolutionStatus=i["contactResolutionStatus"]
-        )
-        db.session.add(contact)
-    for j in alerts:
-        alert = AlertsModel(
-        errorId=j["errorId"],
-        errorSeverity=j["errorSeverity"],
-        errorCategory=j["errorCategory"],
-        errorMessage=j["errorMessage"],
-        longMessage=j["longMessage"],
-        errorTime=j["errorTime"],
-        selected=j["selected"],
-        new=j["new"],
-        expanded=j["expanded"],
-        )
-        db.session.add(alert)
-    db.session.commit()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
+#     contacts = json.load(open("../contacts.json"))
+#     alerts = json.load(open("../alerts.json"))
+#     for i in contacts:
+#         contact = ContactsModel(
+#         _id=i["_id"],
+#         contactId=i["contactId"],
+#         contactStatus=i["contactStatus"],
+#         contactName=i["contactName"],
+#         contactGround=i["contactGround"],
+#         contactSatellite=i["contactSatellite"],
+#         contactEquipment=i["contactEquipment"],
+#         contactState=i["contactState"],
+#         contactStep=i["contactStep"],
+#         contactDetail=i["contactDetail"],
+#         contactBeginTimestamp=i["contactBeginTimestamp"],
+#         contactEndTimestamp=i["contactEndTimestamp"],
+#         contactLatitude=i["contactLatitude"],
+#         contactLongitude=i["contactLongitude"],
+#         contactAzimuth=i["contactAzimuth"],
+#         contactElevation=i["contactElevation"],
+#         contactResolution=i["contactResolution"],
+#         contactResolutionStatus=i["contactResolutionStatus"]
+#         )
+#         db.session.add(contact)
+#     for j in alerts:
+#         alert = AlertsModel(
+#         errorId=j["errorId"],
+#         errorSeverity=j["errorSeverity"],
+#         errorCategory=j["errorCategory"],
+#         errorMessage=j["errorMessage"],
+#         longMessage=j["longMessage"],
+#         errorTime=j["errorTime"],
+#         selected=j["selected"],
+#         new=j["new"],
+#         expanded=j["expanded"],
+#         )
+#         db.session.add(alert)
+#     db.session.commit()
 
 # helper to return objects as dictionaries
 def object_as_dict(obj):
