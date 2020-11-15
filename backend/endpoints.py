419916b9b251ec0351230d01d414d3a37edbec70
Joel Perez <joelness03@gmail.com>
2020-11-14 23:54:19 -0700

    added checking for key existence

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -8,43 +8,61 @@ from models import db, UserModel
 class Signup(Resource):
     def post(self):
         if request.is_json:
-            email = request.json["email"]
-            password = request.json["password"]
+            if "email" in request.json and "password" in request.json:
+                email = request.json["email"]
+                password = request.json["password"]
+            else:
+                email = None
         else:
-            email = request.form["email"]
-            password = request.form["password"]
+            if "email" in request.json and "password" in request.json:
+                email = request.form["email"]
+                password = request.form["password"]
+            else:
+                email = None
         # Check if user exists
-        user_test = UserModel.query.filter_by(email=email).first()
-        if user_test:
-            return {"message":"User Already Exists"}, 409
-        else :
-            user = UserModel(email=email)
-            user.set_password(password)
-            db.session.add(user)
-            try:
-                db.session.commit()
-                access_token = create_access_token(identity=email)
-                return {"message":"Account Successfully Created", "access_token":access_token}, 201
-            except:
-                db.session.rollback()
-                return {"message":"Internal Server Error"}, 500
+        if email and password:
+            user_test = UserModel.query.filter_by(email=email).first()
+            if user_test:
+                return {"message":"User Already Exists"}, 409
+            else :
+                user = UserModel(email=email)
+                user.set_password(password)
+                db.session.add(user)
+                try:
+                    db.session.commit()
+                    access_token = create_access_token(identity=email)
+                    return {"message":"Account Successfully Created", "access_token":access_token}, 201
+                except:
+                    db.session.rollback()
+                    return {"message":"Internal Server Error"}, 500
+        else:
+            return {"message": "Incorrect request"}, 401
 
 
 class Login(Resource):
     def post(self):
         if request.is_json:
-            email = request.json["email"]
-            password = request.json["password"]
-        else:
-            email = request.form["email"]
-            password = request.form["password"]
-        # Find User Record
-        user = UserModel.query.filter_by(email=email).first()
-        if user:
-            if user.check_password(password):
-                access_token = create_access_token(identity=email)
-                return {"message":"Login Success", "access_token":access_token}, 201
+            if "email" in request.json and "password" in request.json:
+                email = request.json["email"]
+                password = request.json["password"]
             else:
-                return {"message":"Email or Password Incorrect"}, 401
+                email = None
         else:
-            return {"message": "No user found with requested email"}, 401
+            if "email" in request.json and "password" in request.json:
+                email = request.form["email"]
+                password = request.form["password"]
+            else:
+                email = None
+        if email:
+            # Find User Record
+            user = UserModel.query.filter_by(email=email).first()
+            if user:
+                if user.check_password(password):
+                    access_token = create_access_token(identity=email)
+                    return {"message":"Login Success", "access_token":access_token}, 201
+                else:
+                    return {"message":"Email or Password Incorrect"}, 401
+            else:
+                return {"message": "No user found with requested email"}, 401
+        else:
+            return {"message": "Incorrect request"}, 401
