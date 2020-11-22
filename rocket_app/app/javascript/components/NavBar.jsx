dacbcc4fbc0f2719ffc862f4086fe5a3e28bab52
ccurnuck <calvin.curnuck@gmail.com>
2020-11-21 21:30:34 -0500

    Added Logout button / minor css

--- rocket_app/app/javascript/components/NavBar.jsx
+++ rocket_app/app/javascript/components/NavBar.jsx
@@ -1,14 +1,24 @@
-// Local Imports
+// Library Imports
 import * as React from "react";
 
+// Local Imports
 import { RuxGlobalStatusBar } from '@astrouxds/rux-global-status-bar/rux-global-status-bar.js';
+import { RuxButton } from '@astrouxds/rux-button/rux-button.js';
+import { RuxClock } from '@astrouxds/rux-clock/rux-clock.js';
 
-const NavBar = () => {
+
+const NavBar = (props) => {
+
+  const { currentUser, logout } = props;
+
+  const logoutBtn = (!!currentUser || currentUser.id === null) ? (<div></div>) : (
+    <rux-button onClick={logout}>Logout</rux-button>
+  )
+
   return (
-    <rux-global-status-bar class="dark-theme" appname="Rocket App" version="3.0">
-      {/* <rux-clock></rux-clock>
-      <div><!-- Any HTML here --></div>
-      <rux-button>Master Off</rux-button> */}
+    <rux-global-status-bar class="dark-theme nav-bar-header" appname="Rocket App">
+      <rux-clock timezone="America/New_York" />
+      {logoutBtn}
     </rux-global-status-bar>
   )
 }
