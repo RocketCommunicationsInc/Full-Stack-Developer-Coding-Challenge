ece0775eb42a78e73512f79051f2420b3d40a380
ccurnuck <calvin.curnuck@gmail.com>
2020-11-23 15:04:32 -0500

    Added Loading Component

---
+++ rocket_app/app/javascript/components/Loading.jsx
@@ -0,0 +1,12 @@
+// Library Imports
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="rux-progress spinner">
+      <progress></progress>
+    </div>
+  )
+}
+
+export default Loading;
