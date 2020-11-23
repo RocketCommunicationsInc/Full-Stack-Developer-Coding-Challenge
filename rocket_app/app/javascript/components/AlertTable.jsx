89820bfd7728e9574f58727c872de496f30c7502
ccurnuck <calvin.curnuck@gmail.com>
2020-11-23 15:03:44 -0500

    Added Stats / headers / styling

--- rocket_app/app/javascript/components/AlertTable.jsx
+++ rocket_app/app/javascript/components/AlertTable.jsx
@@ -2,16 +2,19 @@
 import React from "react";
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
-// import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-
-// Local Imports
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 
 const Table = (props) => {
   const { data } = props;
 
   return (
-    <div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
+    <div className="ag-theme-alpine-dark" style={ { height: 500, width: 600 } }>
+      <div className="table-stats-container">
+        <h2>Alerts</h2>
+      </div>
       <AgGridReact
+        pagination={true}
+        paginationPageSize={25}
         rowData={data}>
         <AgGridColumn field="errorCategory" sortable={true}></AgGridColumn>
         <AgGridColumn field="errorMessage"></AgGridColumn>
--- rocket_app/app/javascript/components/ContactTable.jsx
+++ rocket_app/app/javascript/components/ContactTable.jsx
@@ -1,16 +1,59 @@
 // Library Imports
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { RuxButton } from '@astrouxds/rux-button/rux-button.js';
+import { RuxStatus } from '@astrouxds/rux-status/rux-status.js';
 import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 
-// Local Imports
+const buildStatusCounts = (contacts) => {
+  const counts = { normal: 0, caution: 0, serious: 0, critical: 0 };
+
+  for (let contact of contacts) {
+    let status = contact.contactStatus;
+    if (status in counts) {
+      counts[status]++;
+    }
+  }
+
+  return counts;
+}
+
 
 const Table = (props) => {
+  const [statusCounts, setStatusCounts] = useState({});
   const { data } = props;
+  
+  useEffect(() => {
+    setStatusCounts(buildStatusCounts(data));
+  }, [])
+  
+  const statsBar = Object.keys(statusCounts).map((status, i) => {
+    let newString = status[0].toUpperCase() + status.slice(1);
+
+    let count = statusCounts[status];
+    return (
+      <div key={i} className="table-stat">
+        <rux-status status={status} className=""></rux-status>
+        <p className="table-stat-text">{`${newString}: ${count}`}</p>
+      </div>
+    )
+  });
 
   return (
-    <div className="ag-theme-alpine" style={ { height: 500, width: 600 } }>
+    <div className="ag-theme-alpine-dark" style={ { height: 500, width: 600 } }>
+      <div className="table-stats-container">
+        <h2>Contacts</h2>
+        <section className="table-stats">
+          <div className="table-stat">
+             <p className="table-stat-text">Total Contacts: {data.length}</p>
+          </div>
+          {statsBar}
+        </section>
+      </div>
       <AgGridReact
+        pagination={true}
+        paginationPageSize={25}
         rowData={data}>
         <AgGridColumn field="contactName" sortable={true}></AgGridColumn>
         <AgGridColumn field="contactStatus"></AgGridColumn>
