f6307b779882a3d95803b209a602eceade59f788
Mark <m.hern1415@gmail.com>
2020-11-30 13:38:48 -0800

    undo domain change

--- challenge-api/config/initializers/session_store.rb
+++ challenge-api/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_challenge-api', domain: 'https://rocket-comm.herokuapp.com/'
+    Rails.application.config.session_store :cookie_store, key: '_challenge-api', domain: 'your-frontend-domain'
   else
     Rails.application.config.session_store :cookie_store, key: '_challenge-api' 
 end
