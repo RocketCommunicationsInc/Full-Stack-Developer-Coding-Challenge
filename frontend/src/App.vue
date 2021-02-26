f273166906ad1adb3b93f8b027a46fffc56e7549
Mark Anthony Cianfrani <mark@mile6.com>
2021-02-26 13:43:32 -0500

    build: nvmrc and postcss config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="bg-blue-500">
+  <div id="app" class="bg-blue-500 dark-theme">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import './index.css';
 Vue.config.productionTip = false
 
 import './assets/astro.css'
+import './assets/astro.core.css'
 
 new Vue({
   render: h => h(App),
