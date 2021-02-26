338861981dcf65043f36205f16929effce16e397
ophza <jonathanlet@icloud.com>
2021-02-26 11:05:54 -0700

    Completed frontend for login page

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import logo from '../logo.png';
+import { RuxButton } from '@astrouxds/rux-button/rux-button.js';
+import "../css/login.css";
 
-function login(){
-   return(
-      <div>
-         <h1>placeholder</h1>
-      </div>
-   )
+class login extends Component{
+   state = {
+      username: "",
+      password: "",
+   }
+
+   render(){
+      return(
+         <div>
+            <img className="logo" src={logo} alt="Logo"/>
+            <form className="login">
+               <input name="username" value={this.state.username} placeholder="Enter username" onChange={this.handleInputChange}/>
+               <input name="password" value={this.state.password} placeholder="Enter password" onChange={this.handleInputChange}/>
+               <rux-button onClick={this.login}>Login</rux-button>
+               <rux-button onClick={this.register}>New User?</rux-button>
+            </form>
+         </div>
+      )
+   }
 }
 
 export default login;
