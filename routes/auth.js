a623f60aab8576d1d031ec045edac0c9041cc7cb
ophza <jonathanlet@icloud.com>
2021-02-26 14:37:04 -0700

    Added code to hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/user');
+const bcrypt = require('bcryptjs');
 const {registerValidation} = require('../validation');
 
 router.post('/register', (req, res) => {
@@ -11,10 +12,14 @@ router.post('/register', (req, res) => {
    const nameExist = await User.findOne({name: req.body.name})
    if(nameExist) return res.status(400).send('Username already exists');
 
+   // Hash passwords
+   const salt = await bcrypt.gentSalt(10);
+   const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
    // Creating a new user
    const user = new User({
       name: req.body.name,
-      password: req.body.password
+      password: hashedPassword
    });
    try{
       const savedUser = await user.save();
