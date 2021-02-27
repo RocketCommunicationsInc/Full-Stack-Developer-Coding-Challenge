cad36658ed400806821da8aafb0d915dead93bde
Mark Anthony Cianfrani <mark@mile6.com>
2021-02-27 17:17:26 -0500

    feat: login view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,27 +5,32 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
     {
         path: '/dashboard',
         name: 'Dashboard',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/ViewDashboard.vue')
+        component: () => import(/* webpackChunkName: "dashboard" */ '../views/ViewDashboard.vue')
     },
-  {
-    path: '/register',
-    name: 'Register',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ViewRegister.vue')
-  }
+    {
+        path: '/register',
+        name: 'Register',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "register" */ '../views/ViewRegister.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import(/* webpackChunkName: "login" */ '../views/ViewLogin.vue')
+    }
 ]
 
 const router = new VueRouter({
---
+++ frontend/src/views/ViewLogin.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <base-panel title="Login">
+                <div class="py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form
+                        class="space-y-6"
+                        action="#"
+                        method="POST"
+                        @submit.prevent="submitForm"
+                    >
+                        <rux-input
+                            id="email"
+                            v-model="form.email"
+                            label="Email"
+                            type="email"
+                        />
+                        <rux-input
+                            id="password"
+                            v-model="form.password"
+                            label="Password"
+                            type="password"
+                        />
+
+
+                        <div class="flex items-center">
+                            <rux-button
+                                size="large"
+                                class="ml-auto mr-4 "
+                                :outline="true"
+                                type="default"
+                                @click.native="goToRegister"
+                            >
+                                Register
+                            </rux-button>
+                            <rux-button
+                                size="large"
+                                type="submit"
+                            >
+                                Login
+                            </rux-button>
+                        </div>
+                    </form>
+                </div>
+            </base-panel>
+        </div>
+    </div>
+</template>
+
+<script>
+import BasePanel from "@/components/BasePanel";
+import RuxInput from "@/components/RuxInput";
+import RuxButton from "@/components/RuxButton";
+export default {
+    name: "ViewLogin",
+    components: {RuxButton, RuxInput, BasePanel},
+    data() {
+        return {
+            form: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+
+    methods: {
+        goToRegister() {
+            this.$router.push('/register')
+        },
+        submitForm() {
+            console.log('heard submit')
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
