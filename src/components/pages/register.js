e7cd64b3cd3298c5c590d304b8d6f457e45350b6
ophza <jonathanlet@icloud.com>
2021-02-27 16:39:20 -0700

    Change registration page to have email and a confirm password field

--- src/components/pages/register.js
+++ src/components/pages/register.js
@@ -7,7 +7,9 @@ import { BrowserRouter, Link } from 'react-router-dom';
 class register extends Component{
    state = {
       username: "",
+      email: "",
       password: "",
+      password2: "",
    }
 
    handleNameChange = (event) => {
@@ -15,9 +17,19 @@ class register extends Component{
       this.setState({username: value});
    }
 
+   handleEmailChange = (event) => {
+      const {value} = event.target;
+      this.setState({email: value});
+   }
+
    handlePassChange = (event) => {
       const {value} = event.target;
       this.setState({password: value});
+   }
+
+   handlePassChange2 = (event) => {
+      const {value} = event.target;
+      this.setState({password2: value});
    }
 
    render(){
@@ -26,8 +38,10 @@ class register extends Component{
             <img className="logo" src={logo} alt="Logo"/>
             <form className="login">
                <h1>REGISTER</h1>
-               <input name="username" value={this.state.username} placeholder="Enter username" onChange={this.handleInputChange}/>
+               <input name="username" value={this.state.username} placeholder="Enter username" onChange={this.handleNameChange}/>
+               <input name="email" value={this.state.email} placeholder="Enter email" onChange={this.handleEmailChange}/>
                <input name="password" type="password" value={this.state.password} placeholder="Enter password" onChange={this.handlePassChange}/>
+               <input name="password2" type="password" value={this.state.password2} placeholder="Confirm password" onChange={this.handlePassChange2}/>
                <rux-button onClick={this.register}>Register</rux-button>
                <rux-button><Link to="/">Returning User?</Link></rux-button>
             </form>
