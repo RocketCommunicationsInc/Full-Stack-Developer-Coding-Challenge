aecf14b503eb6c6c649b3081eb5f58082001d918
Mark Anthony Cianfrani <mark@mile6.com>
2021-02-27 21:16:44 -0500

    feat: show error message if data fetch fails

--- frontend/src/components/PanelContacts.vue
+++ frontend/src/components/PanelContacts.vue
@@ -1,32 +1,46 @@
 <template>
     <div class="flex flex-col overflow-hidden">
-        <div class="bg-gray-600 pb-8">
-            <ul class="flex items-center justify-between w-1/3 m-auto">
-                <li class="mr-4">
-                    <base-badge
-                        label="Contacts"
-                        :value="totalContacts"
-                    />
-                </li>
+        <template v-if="!loading">
+            <template v-if="!error">
+                <div class="bg-gray-600 pb-8">
+                    <ul class="flex items-center justify-between w-1/3 m-auto">
+                        <li class="mr-4">
+                            <base-badge
+                                label="Contacts"
+                                :value="totalContacts"
+                            />
+                        </li>
 
-                <li
-                    v-for="(groupedContacts, state) in contactsGroupedByState"
-                    :key="state"
-                    class="mr-4"
-                >
-                    <base-badge
-                        :label="state"
-                        :value="groupedContacts.length"
+                        <li
+                            v-for="(groupedContacts, state) in contactsGroupedByState"
+                            :key="state"
+                            class="mr-4"
+                        >
+                            <base-badge
+                                :label="state"
+                                :value="groupedContacts.length"
+                            />
+                        </li>
+                    </ul>
+                </div>
+                <div class="overflow-auto">
+                    <rux-table
+                        class=""
+                        :columns="columns"
+                        :data="contacts"
                     />
-                </li>
-            </ul>
-        </div>
-        <div class="overflow-auto">
-            <rux-table
-                class=""
-                :columns="columns"
-                :data="contacts"
-            />
+                </div>
+            </template>
+            <div class="p-12 flex justify-center items-center" v-else>
+                {{error}}
+                <rux-button class="ml-4" @click.native="fetchContacts">Retry</rux-button>
+            </div>
+        </template>
+        <div
+            v-else
+            class="p-12"
+        >
+            <base-spinner class="m-auto w-16 h-16" />
         </div>
     </div>
 </template>
@@ -36,13 +50,16 @@ import client from "../../utils/client";
 import RuxTable from "@/components/RuxTable";
 import BaseBadge from "@/components/BaseBadge";
 import { groupBy } from 'lodash'
+import BaseSpinner from "@/components/BaseSpinner";
+import RuxButton from "@/components/RuxButton";
 
 export default {
 name: "PanelContacts",
-    components: {BaseBadge, RuxTable},
+    components: {RuxButton, BaseSpinner, BaseBadge, RuxTable},
     data() {
         return {
-            loading: false,
+            loading: true,
+            error: null,
             contacts: [],
             columns: [
                 {
@@ -82,13 +99,14 @@ name: "PanelContacts",
     },
     methods: {
         async fetchContacts() {
+            this.error = null
             this.loading = true;
             client.get('/contacts')
                 .then(r => {
                     this.contacts = r.data.data
                 })
-                .catch(e => {
-                    console.log(e)
+                .catch(() => {
+                    this.error = 'Unable to fetch Contact data'
                 })
                 .finally(() => {
                     this.loading = false
