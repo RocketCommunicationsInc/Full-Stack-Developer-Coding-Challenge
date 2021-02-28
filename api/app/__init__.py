12478c2aa8c0d36d8cb80e7a4090d6e3bce2a688
Chris B <cbski4@live.com>
2021-02-28 12:11:10 -0600

    feature: ui authentication infrastructure and enable cors in api

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask 
+from flask_cors import CORS
 
 app = Flask(__name__, instance_relative_config=True)
-
-from app import resources
+cors = CORS(app)
 
 app.config.from_object('config')
