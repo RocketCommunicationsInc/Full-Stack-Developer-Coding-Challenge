078b439cba0d37a7a8c44dc0ff2f7223ea076615
ophza <jonathanlet@icloud.com>
2021-02-27 17:33:30 -0700

    Removal of uneeded files, and creating way for front end to communicate with backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const User = require('../model/user');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const {registerValidation, loginValidation} = require('../validation');
 
 // Register
-router.post('/register', async (req, res) => {
+const register =  router.post('/register', async (req, res) => {
    // Validating the data
    const {error} = registerValidation(req.body);
    if(error) return res.status(400).send(error.details[0].message);
@@ -38,7 +37,7 @@ router.post('/register', async (req, res) => {
 });
 
 // Login
-router.post('/login', async (req, res) => {
+const login = router.post('/login', async (req, res) => {
    // Validating the data
    const {error} = loginValidation(req.body);
    if(error) return res.status(400).send(error.details[0].message);
@@ -51,9 +50,9 @@ router.post('/login', async (req, res) => {
    const validPass = await bcrypt.compare(req.body.password, user.password);
    if(!validPass) return res.status(400).send('Email or password is wrong')
 
-   // Create and assign a token
-   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-   res.header('auth-token', token).send(token);
+   // Sending user to main page
+   res.redirect('/main')
 })
 
-module.exports = router;
+module.exports.register = register;
+module.exports.login = login;
--- routes/privateRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const verify = require('./verifyToken');
-
-router.get('/loggedIn', verify,  (req, res) => {
-   return res.redirect('/main')
-})
-
-module.exports = router;
