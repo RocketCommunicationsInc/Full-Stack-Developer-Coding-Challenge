43acc62cdb7334b80f97c5ddb24c75d11b1cf064
ophza <jonathanlet@icloud.com>
2021-02-28 15:50:47 -0700

    Server can now connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+// Set up server
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
-app.get('/test', (req, res) => {
-   res.send('It works');
+// Connect to mongoDB
+mongoose.connect(process.env.DB_CONNECT, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+}, (err) => {
+   if(err) return console.error(err);
+   console.log('Connected to MongoDB');
 });
