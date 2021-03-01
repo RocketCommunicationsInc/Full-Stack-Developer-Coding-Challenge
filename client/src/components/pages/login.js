67a94c88b67b60bdf4015e04a20136c48cdf287c
ophza <jonathanlet@icloud.com>
2021-03-01 13:38:36 -0700

    Created functions for user to be able to login with an existing account

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,39 +1,56 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import logo from '../logo.png';
 import '@astrouxds/rux-button/rux-button.js';
 import '../css/login.css';
 import {Link} from 'react-router-dom';
 
-class Login extends Component{
-   state = {
-      email: "",
-      password: "",
+function Login(){
+
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+   
+   async function login(e) {
+      e.preventDefault();
+
+      try{
+         const loginData = {
+            email, 
+            password,
+         };
+
+         await axios.post(
+            "http://localhost:3001/auth/login", 
+            loginData,
+            {withCredentials: true},
+         )
+      }catch(err){
+         console.error(err);
+      }
    }
 
-   handleEmailChange = (event) => {
-      const {value} = event.target;
-      this.setState({email: value});
-   }
-
-   handlePassChange = (event) => {
-      const {value} = event.target;
-      this.setState({password: value});
-   }
-
-   render(){
-      return(
-         <div>
-            <img className="logo" src={logo} alt="Logo"/>
-            <form className="login">
-               <h1>LOGIN</h1>
-               <input name="email" value={this.state.email} placeholder="Enter email" onChange={this.handleEmailChange}/>
-               <input name="password" type="password" value={this.state.password} placeholder="Enter password" onChange={this.handlePassChange}/>
-               <rux-button><Link to="/main">Login</Link></rux-button>
-               <rux-button><Link to="/register">New User?</Link></rux-button>
-            </form>
-         </div>
-      )
-   }
+   return(
+      <div>
+         <img className="logo" src={logo} alt="Logo"/>
+         <form className="login">
+            <h1>LOGIN</h1>
+            <input 
+               type="email" 
+               placeholder="Enter email" 
+               onChange={(e) => setEmail(e.target.value)} 
+               value={email}
+            />
+            <input 
+               type="password" 
+               placeholder="Enter password" 
+               onChange={(e) => setPassword(e.target.value)} 
+               value={password}
+            />
+            <rux-button onClick={login}>Login</rux-button>
+            <rux-button><Link to="/register">New User?</Link></rux-button>
+         </form>
+      </div>
+   )
 }
 
 export default Login;
