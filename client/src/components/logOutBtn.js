04f0bcb140f644aefa041e05368955a484ba6ba2
ophza <jonathanlet@icloud.com>
2021-03-01 17:16:59 -0700

    Updated functionallity of logout button

--- client/src/components/logOutBtn.js
+++ client/src/components/logOutBtn.js
@@ -1,14 +1,17 @@
 import React, {useContext} from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 import '@astrouxds/rux-button/rux-button.js';
 import AuthContext from './authContext';
 
 function LogOutBtn(){
-
    const {getLoggedIn} = useContext(AuthContext)
+   const history = useHistory();
 
    async function logOut(){
       await axios.get("http://localhost:3001/auth/logout")
-      getLoggedIn();
+      await getLoggedIn();
+      history.push("/");
    }
 
    return <>
