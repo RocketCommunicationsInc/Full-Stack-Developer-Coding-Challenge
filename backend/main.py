94679626e390617cbfe1388b80251bd59890593a
Matt Elsey <matt.elsey@morelandconnect.com>
2021-03-16 11:13:11 -0400

    data reset endpoint and user routes

--- backend/main.py
+++ backend/main.py
@@ -38,12 +38,72 @@ def get_db():
     finally:
         db.close()
 
+def on_after_register(user: UserDB, request: Request):
+    print(f"User {user.id} has registered.")
+
+
+def on_after_forgot_password(user: UserDB, token: str, request: Request):
+    print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+
+def after_verification_request(user: UserDB, token: str, request: Request):
+    print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+#set up routes for user CRUD and Auth
+app.include_router(
+    fastapi_users.get_auth_router(jwt_authentication), prefix="/auth", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(on_after_register), prefix="/auth", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(
+        SECRET, after_forgot_password=on_after_forgot_password
+    ),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(
+        SECRET, after_verification_request=after_verification_request
+    ),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
 
 
 @app.get("/", tags=["root"])
 def read_root() -> dict:
     return { "message": "GRM Dashboard" }
 
+#reset sample data
+@app.get("/reset")
+def resetData(db: Session = Depends(get_db)):
+    
+    message = ""
+
+    #reset alerts table and reset with test data
+    db.execute("DELETE FROM alerts")
+    alertData = json.loads(open('./data/alerts.json').read())
+    statement1 = text("""INSERT INTO alerts (errorId, errorSeverity, errorCategory, errorMessage, longMessage, errorTime, selected, new, expanded) VALUES(:errorId, :errorSeverity, :errorCategory, :errorMessage, :longMessage, :errorTime, :selected, :new, :expanded)""")
+    for line1 in alertData:
+        db.execute(statement1, line1)
+
+    #reset contacts table and reset with test data
+    db.execute("DELETE FROM contacts")
+    contactData = json.loads(open('./data/contacts.json').read())
+    statement2 = text("""INSERT INTO contacts (_id, contactId, contactStatus, contactName, contactGround, contactSatellite, contactEquipment, contactState, contactStep, contactDetail, contactBeginTimestamp, contactEndTimestamp, contactLatitude, contactLongitude, contactAzimuth, contactElevation, contactResolution, contactResolutionStatus) VALUES (:_id, :contactId, :contactStatus, :contactName, :contactGround, :contactSatellite, :contactEquipment, :contactState, :contactStep, :contactDetail, :contactBeginTimestamp, :contactEndTimestamp, :contactLatitude, :contactLongitude, :contactAzimuth, :contactElevation, :contactResolution, :contactResolutionStatus)""")
+    for line2 in contactData:
+        db.execute(statement2, line2)
+
+    try:
+        db.commit()
+        message = "Data reset SUCCESSFUL"
+    except:
+        message = "Data reset FAILED"
+
+    return message
 
 #get all alerts
 @app.get("/alerts")
