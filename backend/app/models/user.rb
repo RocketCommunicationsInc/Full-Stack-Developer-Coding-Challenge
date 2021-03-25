49e8c5620f4b03eb70e2f1876bf5ff7d083af11e
AlexanderBowers <7319abowers@gmail.com>
2021-03-25 15:26:16 -0700

    created jwt backend

---
+++ backend/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :username, uniqueness: true
+
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 end
