197dbe88f047d6fd5a96d83208ecd954ae664392
mimirandi21 <mimirandi21@gmail.com>
2021-04-02 18:33:14 -0500

    redux devtool issue

--- rocketfront/src/redux/store.js
+++ rocketfront/src/redux/store.js
@@ -15,7 +15,9 @@ const store = createStore(
 	rootReducer,
 	compose(
 		applyMiddleware(thunk, logger),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		window.__REDUX_DEVTOOLS_EXTENSION__
+			? window.__REDUX_DEVTOOLS_EXTENSION__()
+			: (f) => f
 	)
 );
 
