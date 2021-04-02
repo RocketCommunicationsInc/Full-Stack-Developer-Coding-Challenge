b082fc28d46ba0340be06f9b93716e7378011dfe
mimirandi21 <mimirandi21@gmail.com>
2021-04-02 06:54:45 -0500

    update cors

--- rocket/config/initializers/cors.rb
+++ rocket/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-        origins 'http://localhost:3000' || 'https://festive-babbage-269a97.netlify.app'
+        origins '*'
     
         resource '*',
             headers: :any,
