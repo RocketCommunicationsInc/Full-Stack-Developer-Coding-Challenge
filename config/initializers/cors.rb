06f7b001483419983b81bcb304bab6eac6936299
David Jaworski <jaws360@gmail.com>
2021-04-03 08:25:02 -0500

    updated cors file origins so it looks for front end host url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://rocket-comms-code-challenge.web.app/'
 
     resource '*',
       headers: :any,
