7d579ff31dcbc141a3b91fcce6ce90d8438c787d
Lizzie <lchan217@gmail.com>
2021-04-03 10:48:37 -0600

    convert app to api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-    resources :contacts, only: [:index]
-    resources :alerts, only: [:index]
+    scope '/api' do
+        resources :contacts, only: [:index]
+        resources :alerts, only: [:index]
+        resources :users, only: [:create]
+
+        post '/login', to: 'sessions#create'
+        delete '/logout', to: 'sessions#destroy'
+        get '/logged_in', to: 'sessions#is_logged_in?'
+    end
 end
