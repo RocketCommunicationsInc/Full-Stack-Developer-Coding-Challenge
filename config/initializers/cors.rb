e374319965f1e2e92a5abdc441e112adc2c30f58
David Jaworski <jaws360@gmail.com>
2021-04-03 08:30:51 -0500

    another attempt without the forward slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://rocket-comms-code-challenge.web.app/'
+    origins 'https://rocket-comms-code-challenge.web.app'
 
     resource '*',
       headers: :any,
