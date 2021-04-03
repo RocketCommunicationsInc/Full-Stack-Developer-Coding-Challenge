ee2cf7fb33915760316497e535bb771bd159a60f
NicDominguez <NicDominguez@users.noreply.github.com>
2021-04-03 16:40:15 -0700

    sets up tabs component in contacts table

--- client/src/components/AlertsTable.js
+++ client/src/components/AlertsTable.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { alertsTestData } from "../alertsTestData";
 import { RuxAccordion } from "@astrouxds/rux-accordion/rux-accordion.js";
+import { RuxStatus } from "@astrouxds/rux-status/rux-status.js";
 
 function AlertsTable() {
   return (
     <div>
+      <h1>Alerts</h1>
       <rux-accordion disabled>
         <span slot="label">Message</span>
         <span slot="label">Category</span>
@@ -14,7 +16,9 @@ function AlertsTable() {
         return (
           <div key={alert.errorId}>
             <rux-accordion>
-              <span slot="label">{alert.errorSeverity}</span>
+              <span slot="label">
+                <rux-status status={alert.errorSeverity}></rux-status>
+              </span>
               <span slot="label">{alert.errorMessage}</span>
               <span slot="label">{alert.errorCategory}</span>
               <span slot="label">{`${new Date(alert.errorTime)}`}</span>
--- client/src/components/ContactsTable.js
+++ client/src/components/ContactsTable.js
@@ -1,33 +1,50 @@
 import React from "react";
 import { contactsTestData } from "../contactsTestData";
 import { RuxAccordion } from "@astrouxds/rux-accordion/rux-accordion.js";
+import { RuxStatus } from "@astrouxds/rux-status/rux-status.js";
+import { RuxTabs } from "@astrouxds/rux-tabs/rux-tabs.js";
 
 function ContactsTable() {
   return (
     <div>
-      <rux-accordion disabled>
-        <span slot="label">Name</span>
-        <span slot="label">GS</span>
-        <span slot="label">Equipment String</span>
-        <span slot="label">Status</span>
-        <span slot="label">AOS – LOS</span>
-      </rux-accordion>
-      {contactsTestData.map((contact) => {
-        return (
-          <div key={contact.contactId}>
-            <rux-accordion>
-              <span slot="label">{contact.contactName}</span>
-              <span slot="label">{contact.contactGround}</span>
-              <span slot="label">{contact.contactEquipment}</span>
-              <span slot="label">{`${contact.contactState} (Step: ${contact.contactStep})`}</span>
-              <span slot="label">{`${new Date(
-                contact.contactBeginTimestamp
-              )}-${new Date(contact.contactEndTimestamp)}`}</span>
-              <span slot="content">{contact.contactDetail}</span>
-            </rux-accordion>
-          </div>
-        );
-      })}
+      <rux-tabs id="tab-set-id-1">
+        <rux-tab id="tab-id-1">Contacts</rux-tab>
+        <rux-tab id="tab-id-2">Equipment</rux-tab>
+      </rux-tabs>
+      <rux-tab-panels aria-labelledby="tab-set-id-1">
+        <rux-tab-panel aria-labelledby="tab-id-1">
+          <h1>Current Contacts</h1>
+          <rux-accordion disabled>
+            <span slot="label">Name</span>
+            <span slot="label">GS</span>
+            <span slot="label">Equipment String</span>
+            <span slot="label">Status</span>
+            <span slot="label">AOS – LOS</span>
+          </rux-accordion>
+          {contactsTestData.map((contact) => {
+            return (
+              <div key={contact.contactId}>
+                <rux-accordion>
+                  <span slot="label">
+                    <rux-status status={contact.contactStatus}></rux-status>
+                  </span>
+                  <span slot="label">{contact.contactName}</span>
+                  <span slot="label">{contact.contactGround}</span>
+                  <span slot="label">{contact.contactEquipment}</span>
+                  <span slot="label">{`${contact.contactState} (Step: ${contact.contactStep})`}</span>
+                  <span slot="label">{`${new Date(
+                    contact.contactBeginTimestamp
+                  )}-${new Date(contact.contactEndTimestamp)}`}</span>
+                  <span slot="content">{contact.contactDetail}</span>
+                </rux-accordion>
+              </div>
+            );
+          })}
+        </rux-tab-panel>
+        <rux-tab-panel aria-labelledby="tab-id-2">
+          Equipment panel coming soon...
+        </rux-tab-panel>
+      </rux-tab-panels>
     </div>
   );
 }
