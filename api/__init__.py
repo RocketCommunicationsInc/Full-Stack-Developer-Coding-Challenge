054cd2eb94ad3bd486704b58190e4115aa97ad6c
NicDominguez <NicDominguez@users.noreply.github.com>
2021-04-05 14:49:10 -0700

    update file paths to blueprints

--- api/__init__.py
+++ api/__init__.py
@@ -45,10 +45,10 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
