1b50a68d9fe0a5b521eca719b685326fd029b1df
NicDominguez <NicDominguez@users.noreply.github.com>
2021-04-09 00:35:49 -0700

    removes decorator

--- api/main.py
+++ api/main.py
@@ -3,22 +3,8 @@ from flask_login import login_required, current_user
 from models.contact import Contact
 from models.alert import Alert
 
-from functools import wraps
-from flask import g, request, redirect, url_for
-
 
 main = Blueprint('main', __name__)
-
-
-def check_for_http(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if request.url.startswith('http://'):
-            url = request.url.replace('http://', 'https://', 1)
-            code = 301
-            return redirect(url, code=code)
-        return f(*args, **kwargs)
-    return decorated_function
 
 
 @main.route('/')
@@ -33,7 +19,6 @@ def not_found(e):
 
 @main.route('/api/contacts', methods=['GET'])
 @login_required
-@check_for_http
 def contacts():
     # returns contacts data from database
     response = Contact.get_delete_put_post()
@@ -42,7 +27,6 @@ def contacts():
 
 
 @main.route('/api/alerts', methods=['GET'])
-@check_for_http
 @login_required
 def alerts():
     # returns alerts data from database
